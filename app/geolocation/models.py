9af2eee9ac0f15a8c881c45d0852a4f8c7bd38ce
Stanotech <satach@o2.pl>
2025-03-12 15:36:52 +0100

    init(project): setup models, migrations, gitignore, serializer, and dependencies

---
+++ app/geolocation/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Geolocation(models.Model):
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    country = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.ip_address or self.url
