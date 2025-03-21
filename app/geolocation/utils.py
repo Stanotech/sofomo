f3e0945218bda4543bdeeaf21a3a30f6d1f44b17
Stanotech <satach@o2.pl>
2025-03-22 00:07:36 +0100

    refactor(views): restructure views logic

--- app/geolocation/utils.py
+++ app/geolocation/utils.py
@@ -1,9 +1,17 @@
 import ipaddress
+from urllib.parse import urlparse
 
 
 def is_valid_ip(ip: str) -> bool:
     try:
         ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+    
+def is_valid_url(url: str) -> bool:
+    try:
+        result = urlparse(url)
         return True
     except ValueError:
         return False
